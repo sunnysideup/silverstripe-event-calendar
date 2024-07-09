47eedae4e231aecf221782fe52377a41e875a4d1
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-07-09 22:49:22 +1200

    FIX: fixing calendar issues with being one day late

--- _config.php
+++ _config.php
@@ -1,19 +1,1 @@
 <?php
-
-global $customDateTemplates;
-$customDateTemplates = array(
-/*
- You can modify the date display by assigning new date templates to any of the following
-   date scenarios. Use the above date format keys.
-
-'OneDay' 			=> '$StartMonthNameShort. $StartDayNumberShort, $StartYearLong'
-'SameMonthSameYear' => '$StartMonthNameShort. $StartDayNumberShort - $EndDatNumberShort, $EndYearLong'
-'DiffMonthSameYear' => '$StartMonthNameShort. $StartDayNumberShort - $EndMonthNameShort. $EndDayNumberShort, $EndYearLong'
-'DiffMonthDiffYear' => '$StartMonthNameShort. $StartDayNumberShort, $StartYearLong - $EndMonthNameShort $EndDayNumberShort, $EndYearLong'
-
-'OneDayHeader' 			=> '$StartMonthNameLong $StartDayNumberShort$StartDaySuffix, $StartYearLong'
-'MonthHeader' 			=> '$StartMonthNameLong, $StartYearLong'
-'YearHeader' 				=> '$StartYearLong'
-
-*/
-);
